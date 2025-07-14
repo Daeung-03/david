d585b2199e27d2477c5507d175c3407c8c83ad32
Daeung-03 <eodnd74@yonsei.ac.kr>
2025-07-14 22:15:51 +0900

    Update priority_calculator.py

--- Chapter1/Problem6/priority_calculator.py
+++ Chapter1/Problem6/priority_calculator.py
@@ -15,7 +15,7 @@ def subtract(a, b):
     a-b return
     """
     return a-b
-    
+    ㅉ
 def multiply(a, b):
     """
     a*b return
@@ -45,50 +45,53 @@ def calculate(a, b, operator):
         return multiply(a,b)
     elif operator == '/':
         return divide(a,b)
-    else:
-        print("Invalid operator")
-        exit()
+
 
 def stris_float(string):
-"""
-넘겨받은 인자가 float로 바꿀 수 있는 string인지 확인한다.
-"""
-  try:
-    float(string)
-    return True
-  except ValueError:
-    return False
+    """
+    넘겨받은 인자가 float로 바꿀 수 있는 string인지 확인한다.
+    """
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+def check_valid(list):
+    """
+    입력받은 expression이 타당한지 확인
+    """
+    for data in list:
+        if not stris_float(data) and data not in operator_list: #숫자도 아니고 오퍼레이터도 아니면 error
+            print("Invalid number input.")
+            exit()
 
 def main():
-    try:
-        expression = input().split()
-        numbers = [float(x) for x in expression if stris_float(x)]
-        operators = [x for x in expression if x in operator_list] #숫자와 문자열 나누어 저장
-        index_1th = 0 #곱하기, 나누기 할 숫자 인덱스
-        index_2nd = 0 #더하기, 빼기 할 숫자 인덱스
-        
-        for i in operators: #곱셉, 나눗셈 찾아 연산
-            if i == '*' or i == '/':
-                temp = calculate(numbers[index_1th], numbers[index_1th+1], i)
-                numbers.pop(index_1th)
-                numbers.pop(index_1th)
-                numbers.insert(index_1th, temp)
-            else: index_1th += 1
+    expression = input().split()
+    check_valid(expression)
+    numbers = [float(x) for x in expression if stris_float(x)]
+    operators = [x for x in expression if x in operator_list] #숫자와 문자열 나누어 저장
+    index_1th = 0 #곱하기, 나누기 할 숫자 인덱스
+    index_2nd = 0 #더하기, 빼기 할 숫자 인덱스
+    
+    for i in operators: #곱셉, 나눗셈 찾아 연산
+        if i == '*' or i == '/':
+            temp = calculate(numbers[index_1th], numbers[index_1th+1], i)
+            numbers.pop(index_1th)
+            numbers.pop(index_1th)
+            numbers.insert(index_1th, temp)
+        else: index_1th += 1
+            
                 
-                    
-        for j in operators: #더하기, 빼기 찾아 연산
-            if j == '+' or j == '-':
-                temp = calculate(numbers[index_2nd], numbers[index_2nd+1], j)
-                numbers.pop(index_2nd)
-                numbers.pop(index_2nd)
-                numbers.insert(index_2nd, temp)
-            else: index_2nd += 1
-        
-        print(numbers[0]) #최종적으로 남은 결과값 출력
-
-        
-    except ValueError: #잘못된 수식 예외 처리
-        print("Invalid number input.")
+    for j in operators: #더하기, 빼기 찾아 연산
+        if j == '+' or j == '-':
+            temp = calculate(numbers[index_2nd], numbers[index_2nd+1], j)
+            numbers.pop(index_2nd)
+            numbers.pop(index_2nd)
+            numbers.insert(index_2nd, temp)
+        else: index_2nd += 1
+    
+    print(numbers[0]) #최종적으로 남은 결과값 출력
     
 if __name__ == "__main__":
     main()

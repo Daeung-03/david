2359a6fdb12392dff1be5218d7a32c66645c8acc
Daeung-03 <eodnd74@yonsei.ac.kr>
2025-07-22 10:50:31 +0000

    fix prob7

--- Chapter1/Problem7/david/app.py
+++ Chapter1/Problem7/david/app.py
@@ -49,4 +49,4 @@ def ask():
             return render_template('index.html', error=error, audio = None, download_url = download_url) #오류 발생 페이지 출력
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000, debug = True)
+    app.run('0.0.0.0', port = 5000, debug = True)
